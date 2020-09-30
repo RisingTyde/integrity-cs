36bb8d4923c931df4bd9d26fd8cd0db4d7d56b06
Austin Roberts <auz.tin@outlook.com>
2020-09-30 10:54:25 +0100

    Updated the readme

--- Integrity.cs
+++ Integrity.cs
@@ -4,8 +4,17 @@ namespace IntegrityCheck
 {
     public class Integrity
     {
+        /// <summary>
+        /// Default message for null pointer exceptions. This can be changed if you want a different message.
+        /// </summary>
         public static string nullPointerDefaultMessage = "Encountered Null pointer";
 
+        /// <summary>
+        /// Checks whether a condition is true, if not raises an InvalidOperationException, whose message is either a default message, or built from passed in message params
+        /// </summary>
+        /// <param name="condition">The condition to check is true.</param>
+        /// <param name="mesageArgs">See the documentation for deferredStringBuilder to see how the message is built.</param>
+        /// <exception cref="InvalidOperationException">Raised if condition is false</exception>
         public static void Check(bool condition, params object[] mesageArgs)
         {
             if (!condition)
@@ -15,22 +24,23 @@ namespace IntegrityCheck
             }
         }
 
-        public static void CheckIsBool(bool test, params object[] mesageArgs)
+        /// <summary>
+        /// Raises an InvalidOperationException, whose message is either a default message, or built from passed in message params
+        /// </summary>
+        /// <param name="mesageArgs">See the documentation for deferredStringBuilder to see how the message is built.</param>
+        /// <exception cref="InvalidOperationException"></exception>
+        public static void Fail(params object[] mesageArgs)
         {
-            /* This is just here so that the compiler will chose this version when the argument is not a bool?
-             * This will avoid an auto-boxing conversion from bool to bool?
-             */
+            string text = Integrity.getMessage("Integrity check failed", mesageArgs);
+            throw new InvalidOperationException(text);
         }
 
-        public static void CheckIsBool(bool? test, params object[] mesageArgs)
-        {
-            if(test == null)
-            {
-                string text = Integrity.getMessage("Expected bool, was null", mesageArgs);
-                throw new NullReferenceException(text);
-            }
-        }
-
+        /// <summary>
+        /// Checks whether an object is null, and if so raises an exception with a default message or one built from passed in message params
+        /// </summary>
+        /// <param name="obj">The object to check for nullness.</param>
+        /// <param name="mesageArgs">See the documentation for deferredStringBuilder to see how the message is built.</param>
+        /// <exception cref="NullReferenceException">Raised if obj is null</exception>
         public static void CheckNotNull(object obj, params object[] mesageArgs)
         {
             if (obj == null)
@@ -39,6 +49,16 @@ namespace IntegrityCheck
             }
         }
 
+        /// <summary>
+        /// Checks whether a string is null or zero length. If so, raises an exception with a default message or one built from passed in message params
+        /// </summary>
+        /// <param name="s">The string to check for nullness or zero length.</param>
+        /// <param name="mesageArgs">See the documentation for deferredStringBuilder to see how the message is built.</param>
+        /// <exception cref="NullReferenceException">Raised if s is null</exception>
+        /// <exception cref="InvalidOperationException">Raised if s is exactly 0 characters long</exception>
+        /// <remarks>
+        /// Note that an exception is only raised if the string has exactly zero length; a string with a single space (for example) would be fine
+        /// </remarks>
         public static void CheckStringNotNullOrEmpty(string s, params object[] mesageArgs)
         {
             if (s == null)
@@ -52,12 +72,28 @@ namespace IntegrityCheck
             }
         }
 
+        /// <summary>
+        /// Should not be used for a non-floating point numbers.
+        /// </summary>
+        /// <param name="d">Ignored.</param>
+        /// <param name="mesageArgs">Ignored</param>
+        /// <remarks>
+        /// This only exists because the variants for floating point numbers exist (where it makes sense to check for NaN etc.). Having this overload avoids a conversion to a floating number and some pointless checks. 
+        /// </remarks>
         public static void CheckIsValidNumber(int d, params object[] mesageArgs)
         {
             // speedy shortcut so compiler doesn't waste time converting
         }
 
-
+        /// <summary>
+        /// Should not be used for a non-floating point numbers. Dose, however, ensure the number is not null.
+        /// </summary>
+        /// <param name="d">The number to check for NaN, null, or Infinity.</param>
+        /// <param name="mesageArgs">See the documentation for deferredStringBuilder to see how the message is built.</param>
+        /// <exception cref="NullReferenceException">Raised if d is null</exception>
+        /// <remarks>
+        /// This only exists because the variants for floating point numbers exist (where it makes sense to check for NaN etc.). Having this overload avoids a conversion to a floating number and some pointless checks. 
+        /// </remarks>
         public static void CheckIsValidNumber(int? d, params object[] mesageArgs)
         {
             if (d == null)
@@ -65,10 +101,29 @@ namespace IntegrityCheck
                 throw new NullReferenceException(Integrity.getMessage(nullPointerDefaultMessage, mesageArgs));
             }
         }
+
+        /// <summary>
+        /// Should not be used for a non-floating point numbers.
+        /// </summary>
+        /// <param name="d">Ignored.</param>
+        /// <param name="mesageArgs">Ignored</param>
+        /// <remarks>
+        /// This only exists because the variants for floating point numbers exist (where it makes sense to check for NaN etc.). Having this overload avoids a conversion to a floating number and some pointless checks. 
+        /// </remarks>
         public static void CheckIsValidNumber(uint d, params object[] mesageArgs)
         {
             // speedy shortcut so compiler doesn't waste time converting
         }
+
+        /// <summary>
+        /// Should not be used for a non-floating point numbers. Dose, however, ensure the number is not null.
+        /// </summary>
+        /// <param name="d">The number to check for NaN, null, or Infinity.</param>
+        /// <param name="mesageArgs">See the documentation for deferredStringBuilder to see how the message is built.</param>
+        /// <exception cref="NullReferenceException">Raised if d is null</exception>
+        /// <remarks>
+        /// This only exists because the variants for floating point numbers exist (where it makes sense to check for NaN etc.). Having this overload avoids a conversion to a floating number and some pointless checks. 
+        /// </remarks>
 
         public static void CheckIsValidNumber(uint? d, params object[] mesageArgs)
         {
@@ -77,12 +132,28 @@ namespace IntegrityCheck
                 throw new NullReferenceException(Integrity.getMessage(nullPointerDefaultMessage, mesageArgs));
             }
         }
-
+        /// <summary>
+        /// Should not be used for a non-floating point numbers.
+        /// </summary>
+        /// <param name="d">Ignored.</param>
+        /// <param name="mesageArgs">Ignored</param>
+        /// <remarks>
+        /// This only exists because the variants for floating point numbers exist (where it makes sense to check for NaN etc.). Having this overload avoids a conversion to a floating number and some pointless checks. 
+        /// </remarks>
         public static void CheckIsValidNumber(long d, params object[] mesageArgs)
         {
             // speedy shortcut so compiler doesn't waste time converting 
         }
 
+        /// <summary>
+        /// Should not be used for a non-floating point numbers. Dose, however, ensure the number is not null.
+        /// </summary>
+        /// <param name="d">The number to check for NaN, null, or Infinity.</param>
+        /// <param name="mesageArgs">See the documentation for deferredStringBuilder to see how the message is built.</param>
+        /// <exception cref="NullReferenceException">Raised if d is null</exception>
+        /// <remarks>
+        /// This only exists because the variants for floating point numbers exist (where it makes sense to check for NaN etc.). Having this overload avoids a conversion to a floating number and some pointless checks. 
+        /// </remarks>
         public static void CheckIsValidNumber(long? d, params object[] mesageArgs)
         {
             if (d == null)
@@ -91,6 +162,42 @@ namespace IntegrityCheck
             }
         }
 
+        /// <summary>
+        /// Should not be used for a non-floating point numbers.
+        /// </summary>
+        /// <param name="d">Ignored.</param>
+        /// <param name="mesageArgs">Ignored</param>
+        /// <remarks>
+        /// This only exists because the variants for floating point numbers exist (where it makes sense to check for NaN etc.). Having this overload avoids a conversion to a floating number and some pointless checks. 
+        /// </remarks>
+        public static void CheckIsValidNumber(ulong d, params object[] mesageArgs)
+        {
+            // speedy shortcut so compiler doesn't waste time converting 
+        }
+
+        /// <summary>
+        /// Should not be used for a non-floating point numbers. Dose, however, ensure the number is not null.
+        /// </summary>
+        /// <param name="d">The number to check for NaN, null, or Infinity.</param>
+        /// <param name="mesageArgs">See the documentation for deferredStringBuilder to see how the message is built.</param>
+        /// <exception cref="NullReferenceException">Raised if d is null</exception>
+        /// <remarks>
+        /// This only exists because the variants for floating point numbers exist (where it makes sense to check for NaN etc.). Having this overload avoids a conversion to a floating number and some pointless checks. 
+        /// </remarks>
+        public static void CheckIsValidNumber(ulong? d, params object[] mesageArgs)
+        {
+            if (d == null)
+            {
+                throw new NullReferenceException(Integrity.getMessage(nullPointerDefaultMessage, mesageArgs));
+            }
+        }
+
+        /// <summary>
+        /// Checks whether a number is NaN, +Infinity, -Infinity. If so, raises an exception with a default message or one built from passed in message params
+        /// </summary>
+        /// <param name="d">The number to check for NaN, or +/-Infinity.</param>
+        /// <param name="mesageArgs">See the documentation for deferredStringBuilder to see how the message is built.</param>
+        /// <exception cref="InvalidOperationException">Raised if d is NaN, +/-Infinity</exception>
         public static void CheckIsValidNumber(float d, params object[] mesageArgs)
         {
             if (Double.IsNaN(d))
@@ -111,6 +218,14 @@ namespace IntegrityCheck
                 throw new InvalidOperationException(text);
             }
         }
+        
+        /// <summary>
+        /// Checks whether a number is NaN, +Infinity, -Infinity or null. If so, raises an exception with a default message or one built from passed in message params
+        /// </summary>
+        /// <param name="d">The number to check for NaN, null, or +/-Infinity.</param>
+        /// <param name="mesageArgs">See the documentation for deferredStringBuilder to see how the message is built.</param>
+        /// <exception cref="NullReferenceException">Raised if d is null</exception>
+        /// <exception cref="InvalidOperationException">Raised if d is NaN, +/-Infinity</exception>
         public static void CheckIsValidNumber(float? d, params object[] mesageArgs)
         {
             if (d == null)
@@ -137,6 +252,12 @@ namespace IntegrityCheck
             }
         }
 
+        /// <summary>
+        /// Checks whether a number is NaN, +Infinity, -Infinity. If so, raises an exception with a default message or one built from passed in message params
+        /// </summary>
+        /// <param name="d">The number to check for NaN, or +/-Infinity.</param>
+        /// <param name="mesageArgs">See the documentation for deferredStringBuilder to see how the message is built.</param>
+        /// <exception cref="InvalidOperationException">Raised if d is NaN, +/-Infinity</exception>
         public static void CheckIsValidNumber(double d, params object[] mesageArgs)
         {
             if (Double.IsNaN(d))
@@ -158,6 +279,13 @@ namespace IntegrityCheck
             }
         }
 
+        /// <summary>
+        /// Checks whether a number is NaN, +Infinity, -Infinity or null. If so, raises an exception with a default message or one built from passed in message params
+        /// </summary>
+        /// <param name="d">The number to check for NaN, null, or +/-Infinity.</param>
+        /// <param name="mesageArgs">See the documentation for deferredStringBuilder to see how the message is built.</param>
+        /// <exception cref="NullReferenceException">Raised if d is null</exception>
+        /// <exception cref="InvalidOperationException">Raised if d is NaN, +/-Infinity</exception>
         public static void CheckIsValidNumber(double? d, params object[] mesageArgs)
         {
             if (d == null)
@@ -183,6 +311,12 @@ namespace IntegrityCheck
             }
         }
 
+        /// <summary>
+        /// Checks whether a number is NaN, +Infinity, -Infinity. If so, raises an exception with a default message or one built from passed in message params
+        /// </summary>
+        /// <param name="d">The number to check for NaN, or +/-Infinity.</param>
+        /// <param name="mesageArgs">See the documentation for deferredStringBuilder to see how the message is built.</param>
+        /// <exception cref="InvalidOperationException">Raised if d is NaN, +/-Infinity</exception>
         public static void CheckIsValidNumberOrNull(double? d, params object[] mesageArgs)
         {
             if(d == null)
@@ -192,6 +326,53 @@ namespace IntegrityCheck
             CheckIsValidNumber(d, mesageArgs);
         }
 
+        public static void CheckIsValidNumberOrNull(int d, params object[] mesageArgs)
+        {
+        }
+        public static void CheckIsValidNumberOrNull(int? d, params object[] mesageArgs)
+        {
+        }
+        public static void CheckIsValidNumberOrNull(uint d, params object[] mesageArgs)
+        {
+        }
+        public static void CheckIsValidNumberOrNull(uint? d, params object[] mesageArgs)
+        {
+        }
+        public static void CheckIsValidNumberOrNull(long d, params object[] mesageArgs)
+        {
+        }
+        public static void CheckIsValidNumberOrNull(long? d, params object[] mesageArgs)
+        {
+        }
+        public static void CheckIsValidNumberOrNull(ulong d, params object[] mesageArgs)
+        {
+        }
+        public static void CheckIsValidNumberOrNull(ulong? d, params object[] mesageArgs)
+        {
+        }
+        /// <summary>
+        /// Concatenates and substitutes passed in arguments to form a string.
+        /// Builds a string either by replacing {} in the string being built with the string representation of the next argumnt, or by concatenating ", " plus the string representation.
+        /// Note: if strings are substituted into {} they are substituted in as-is. If they are concatenated then they are surrounded by single quotes to indicate that they are strings.
+        /// </summary>
+        /// <param name="messageArgs">None or many arguments, of any type.</param>
+        /// <returns>
+        /// A string built from the passed in arguments.
+        /// </returns>
+        /// <remarks>
+        /// Examples:
+        /// deferredStringbuilder("this is {}", 1) returns "This is 1"
+        /// deferredStringbuilder("this is", 1) returns "This is, 1"
+        /// deferredStringbuilder(1, 2, 3) returns "1, 2, 3"
+        /// deferredStringbuilder(1, 2, 3) returns "1, 2, 3"
+        /// deferredStringbuilder(true, "str", 1.1) returns "True, 'str', 1.1"
+        /// deferredStringbuilder("This is {}", "weird {} {}", 1, 2) returns "This is weird 1 2"
+        /// deferredStringbuilder("{} {}", 1, 2, 3) returns "1 2, 3"
+        /// </remarks>
+        public static string deferredStringbuilder(params object[] messageArgs)
+        {
+            return getMessage("", messageArgs);
+        }
         private static string getMessage(string defaultMessage, params object[] mesageArgs)
         {
             if (mesageArgs == null || mesageArgs.Length == 0)
@@ -206,13 +387,21 @@ namespace IntegrityCheck
                 int braces = s.IndexOf("{}");
                 if (braces == -1)
                 {
+                    string itemAsString = null;
+                    if(item.GetType() == typeof(string))
+                    {
+                        itemAsString = "'" + item + "'";
+                    } else
+                    {
+                        itemAsString = item.ToString();
+                    }
                     if (s.Length == 0)
                     {
-                        s += item.ToString();
+                        s += itemAsString;
                     }
                     else
                     {
-                        s += ", " + item.ToString();
+                        s += ", " + itemAsString;
                     }
                 }
                 else
--- README.md
+++ README.md
@@ -1,2 +1,68 @@
-# integrity-cs
-Replacement for assert
+# Integrity check
+
+## Installation
+
+The package is published on nuget.org here:
+https://www.nuget.org/packages/integrity-check/
+
+## Integrity API
+
+The full set of functions is:
+
+```JavaScript
+    Integrity.Check(condition, *msg)
+    Integrity.CheckNotNull(object obj, params object[] mesageArgs)
+    Integrity.CheckStringNotNullOrEmpty(string s, params object[] mesageArgs)
+    Integrity.CheckIsValidNumber(variousOverloadedTypes d, params object[] mesageArgs)
+    Integrity.CheckIsValidNumberOrNull(variousOverloadedTypes d, params object[] mesageArgs)
+
+    Integrity.fail((params object[] mesageArgs)
+```
+### Exceptions
+
+The following exceptions are thrown:
+* NullReferenceException - when the parameter being tested is null (and it is not allowed to be)
+* InvalidOperationException - to indicate a check failed
+
+## Examples
+
+```c#
+using IntegrityCheck;
+
+namespace Example
+{
+    class MyClass
+    {
+        int someCondition = 0;
+
+        /// <summary>
+        /// Calculates something really clever
+        /// </summary>
+        /// <param name="f">Must not be NaN, or +- Infinity</param>
+        /// <exception cref="InvalidOperationException"></exception>
+        public float computeSomeNum(float f)
+        {
+            Integrity.CheckIsValidNumber(f);
+
+            float retValue = 0;
+
+            switch(someCondition)
+            {
+                case 1:
+                    // do some complicated stuff...
+                    break;
+                case 2:
+                    // do some complicated stuff...
+                    break;
+                default:
+                    Integrity.Fail("Should never get here, switch value was {}", someCondition);
+                    break;
+            }
+
+            Integrity.CheckIsValidNumber(retValue, "Strange, should not be possible... retValue is {}", retValue);
+
+            return retValue;
+        }
+    }
+}
+```
